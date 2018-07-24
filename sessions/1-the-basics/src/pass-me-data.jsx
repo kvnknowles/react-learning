4fefb70796251d607b24446cce52ce82b205a5d1
Kevin Knowles <knowles.kevin@gmail.com>
2018-07-24 12:57:04 -0500

    Pass the pass-me-data test

--- sessions/1-the-basics/src/pass-me-data.jsx
+++ sessions/1-the-basics/src/pass-me-data.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const PassMeData = () => <span />;
+const PassMeData = props => <span>{GetName(props)}</span>;
+
+const GetName = props => {
+  return props.title + ' ' + props.name;
+};
 
 export default PassMeData;
