0e977dfaa2440c07bc1515bef33044b6a5667c82
Kevin Knowles <knowles.kevin@gmail.com>
2018-07-24 12:27:41 -0500

    Update stateful code to pass tests

--- sessions/1-the-basics/src/stateful.jsx
+++ sessions/1-the-basics/src/stateful.jsx
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
 
 export default class Stateful extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentTime: new Date().toISOString() };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({ currentTime: new Date().toISOString() });
+    }, 1000);
+  }
+
+  // TODO: Understand why this should be done?
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
-    return <div />;
+    return (
+      <div>
+        <span>{this.state.currentTime}</span>
+      </div>
+    );
   }
 }
