3ed03af3c4ffc0c986409daceb70bde2cb94c2ba
Kevin Knowles <knowles.kevin@gmail.com>
2018-07-24 13:01:22 -0500

    pass composition tests

--- sessions/1-the-basics/src/composition.jsx
+++ sessions/1-the-basics/src/composition.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const Composition = () => <div />;
+const Composition = props => (
+  <div>
+    <h1>Composition:</h1>
+    {props.children}
+  </div>
+);
 
 export default Composition;
